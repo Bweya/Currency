75e25d36a612ac1627a59b2dcc6e9b81e068ece3
Bweya Omukhulu <bweya@Bweyas-MacBook-Air.local>
2020-02-20 21:01:07 -0500

    c_2.py

--- c_2.py
+++ c_2.py
@@ -2,14 +2,15 @@ import xlsxwriter
 import pandas as pd
 import os
 import math
+import datetime
 
 def trade():
 
     files = os.listdir('files')
+    year = datetime.datetime.now().year
+    #year = '2019'
 
-    year = '2019'
-
-    workbook = xlsxwriter.Workbook(year+'currency.xlsx')
+    workbook = xlsxwriter.Workbook(str(year)+'currency.xlsx')
     worksheet = workbook.add_worksheet()
 
     border = workbook.add_format({'border':1,'align':'center'})
@@ -17,7 +18,7 @@ def trade():
 
     worksheet.set_column('A:F', 45)
 
-    worksheet.merge_range('A1:F1', 'FX Trades by Currency 01 JAN - 31 DEC '+year, bolds)
+    worksheet.merge_range('A1:F1', 'FX Trades by Currency 01 JAN - 31 DEC '+str(year), bolds)
 
     bold = workbook.add_format({'bold':True, 'border':1})
     gtotal =  workbook.add_format({'bold': True, 'align':'center', 'bg_color':'#A9A9A9', 'border': 1})
@@ -60,21 +61,11 @@ def trade():
     #==========================================================================
 
 
-    #==========================================================================
-
-    #valueAdd_sorted.sort(reverse = True)
-    #v = valueAdd_sorted
-    #for val in v:
-
-
+    valueAdd = []
     for curr in check_curr:
 
         count = 0
         valueadd_bp = 0
-        amount = 0
-        base_market = 0
-        base_CO = 0
-
 
         for file in files:
 
@@ -86,40 +77,80 @@ def trade():
 
                 TradeData = pd.read_excel("files/"+file, sheet_name = 0, header = None, skiprows=3)
 
-                #for x in TradeData[6]:
-                    #if isinstance(x, str) == True:
-
-
                 index = 0
                 for y in TradeData[6]:
                     if isinstance(y, str) == True:
 
                         if curr == y.strip():
-                            count += 1
+
                             valueadd_bp += float( (TradeData[16][index]).replace(",","") )
-                            amount += float( TradeData[5][index].replace(",","") )
-                            base_market += float( TradeData[14][index].replace(",","") )
-                            base_CO += float( TradeData[13][index].replace(",","") )
+
+
                     index += 1
+        valueAdd.append(valueadd_bp)
 
-        #if val ==  valueadd_bp:
 
-        grand_valueAdd += valueadd_bp
-        grand_amount += amount
-        grand_count += count
-        grand_base_CO += base_CO
-        grand_base_market += base_market
 
-                #print(file[:-25],' Grand value add equals: ', "{:,.2f}".format(grand_valueAdd) )
-        worksheet.write("A"+str(row_record), curr, border)
-        worksheet.write("B"+str(row_record), "{:,.2f}".format(amount), border)
-        worksheet.write("C"+str(row_record), "{:,.2f}".format(base_market), border)
-        worksheet.write("D"+str(row_record), "{:,.2f}".format(base_CO), border)
-        worksheet.write("E"+str(row_record), "{:,.2f}".format(count), border)
+    #==========================================================================
 
-        worksheet.write("F"+str(row_record), "{:,.2f}".format(valueadd_bp), border)
+    valueAdd.sort(reverse = True)
+    v = valueAdd
+    for val in v:
 
-        row_record += 1
+
+        for curr in check_curr:
+
+            count = 0
+            valueadd_bp = 0
+            amount = 0
+            base_market = 0
+            base_CO = 0
+
+
+            for file in files:
+
+                if file[:3] == '.DS' or file[:1] == 't':
+
+                    print('DS File Store')
+
+                else:
+
+                    TradeData = pd.read_excel("files/"+file, sheet_name = 0, header = None, skiprows=3)
+
+                    #for x in TradeData[6]:
+                        #if isinstance(x, str) == True:
+
+
+                    index = 0
+                    for y in TradeData[6]:
+                        if isinstance(y, str) == True:
+
+                            if curr == y.strip():
+                                count += 1
+                                valueadd_bp += float( (TradeData[16][index]).replace(",","") )
+                                amount += float( TradeData[5][index].replace(",","") )
+                                base_market += float( TradeData[14][index].replace(",","") )
+                                base_CO += float( TradeData[13][index].replace(",","") )
+                        index += 1
+
+
+            if val == valueadd_bp:
+
+                grand_valueAdd += valueadd_bp
+                grand_amount += amount
+                grand_count += count
+                grand_base_CO += base_CO
+                grand_base_market += base_market
+
+                worksheet.write("A"+str(row_record), curr, border)
+                worksheet.write("B"+str(row_record), "{:,.2f}".format(amount), border)
+                worksheet.write("C"+str(row_record), "{:,.2f}".format(base_market), border)
+                worksheet.write("D"+str(row_record), "{:,.2f}".format(base_CO), border)
+                worksheet.write("E"+str(row_record), "{:,.2f}".format(count), border)
+
+                worksheet.write("F"+str(row_record), "{:,.2f}".format(valueadd_bp), border)
+
+                row_record += 1
 
 
         print('\n')
